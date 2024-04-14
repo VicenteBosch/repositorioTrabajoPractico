355faa396d90df93d901e1c685fb89d4e7a87760
VicenteBosch <vbosch@udesa.edu.ar>
2024-04-14 18:48:58 -0300

    creando la data base, con 10 autos distintos

---
+++ tp/db/index.js
@@ -0,0 +1,135 @@
+const auots = {
+    usuario: {
+        email: "usuario@example.com",
+        usuario: "nombredeusuario",
+        contraseña: "contraseña123",
+        fecha_de_nacimiento: "1990-01-01",
+        nro_de_documento: 123456789,
+        foto_de_perfil: "/img/perfil.jpg" 
+    },
+
+    productos: [
+        {
+            imagen: "/img/auto1.jpg",
+            nombre_producto: "Toyota Corolla",
+            descripcion: "Descripción del Toyota Corolla",
+            comentarios: [
+                {
+                    nombre_usuario: "Usuario1",
+                    texto_comentario: "Comentario sobre Toyota Corolla",
+                    imagen_perfil: "/img/perfil1.jpg"
+                }
+            ]
+        },
+        {
+            imagen: "/img/auto2.jpg",
+            nombre_producto: "Honda Civic",
+            descripcion: "Descripción del Honda Civic",
+            comentarios: [
+                {
+                    nombre_usuario: "Usuario2",
+                    texto_comentario: "Comentario sobre Honda Civic",
+                    imagen_perfil: "/img/perfil2.jpg"
+                }
+            ]
+        },   
+        {
+            imagen: "/img/auto3.jpg",
+            nombre_producto: "Ford Fiesta",
+            descripcion: "Descripción del Ford Fiesta",
+            comentarios: [
+                {
+                    nombre_usuario: "Usuario3",
+                    texto_comentario: "Comentario sobre Ford Fiesta",
+                    imagen_perfil: "/img/perfil3.jpg"
+                }
+            ]
+        },
+        {
+            imagen: "/img/auto4.jpg",
+            nombre_producto: "Chevrolet Cruze",
+            descripcion: "Descripción del Chevrolet Cruze",
+            comentarios: [
+                {
+                    nombre_usuario: "Usuario4",
+                    texto_comentario: "Comentario sobre Chevrolet Cruze",
+                    imagen_perfil: "/img/perfil4.jpg"
+                }
+            ]
+        },   
+        {
+            imagen: "/img/auto5.jpg",
+            nombre_producto: "Nissan Sentra",
+            descripcion: "Descripción del Nissan Sentra",
+            comentarios: [
+                {
+                    nombre_usuario: "Usuario5",
+                    texto_comentario: "Comentario sobre Nissan Sentra",
+                    imagen_perfil: "/img/perfil5.jpg"
+                }
+            ]
+        },
+        {
+            imagen: "/img/auto6.jpg",
+            nombre_producto: "Volkswagen Jetta",
+            descripcion: "Descripción del Volkswagen Jetta",
+            comentarios: [
+                {
+                    nombre_usuario: "Usuario6",
+                    texto_comentario: "Comentario sobre Volkswagen Jetta",
+                    imagen_perfil: "/img/perfil6.jpg"
+                }
+            ]
+        },   
+        {
+            imagen: "/img/auto7.jpg",
+            nombre_producto: "Hyundai Elantra",
+            descripcion: "Descripción del Hyundai Elantra",
+            comentarios: [
+                {
+                    nombre_usuario: "Usuario7",
+                    texto_comentario: "Comentario sobre Hyundai Elantra",
+                    imagen_perfil: "/img/perfil7.jpg"
+                }
+            ]
+        },
+        {
+            imagen: "/img/auto8.jpg",
+            nombre_producto: "Mazda3",
+            descripcion: "Descripción del Mazda3",
+            comentarios: [
+                {
+                    nombre_usuario: "Usuario8",
+                    texto_comentario: "Comentario sobre Mazda3",
+                    imagen_perfil: "/img/perfil8.jpg"
+                }
+            ]
+        },
+        {
+            imagen: "/img/auto9.jpg",
+            nombre_producto: "Subaru Impreza",
+            descripcion: "Descripción del Subaru Impreza",
+            comentarios: [
+                {
+                    nombre_usuario: "Usuario9",
+                    texto_comentario: "Comentario sobre Subaru Impreza",
+                    imagen_perfil: "/img/perfil9.jpg"
+                }
+            ]
+        },
+        {
+            imagen: "/img/auto10.jpg",
+            nombre_producto: "Kia Forte",
+            descripcion: "Descripción del Kia Forte",
+            comentarios: [
+                {
+                    nombre_usuario: "Usuario10",
+                    texto_comentario: "Comentario sobre Kia Forte",
+                    imagen_perfil: "/img/perfil10.jpg"
+                }
+            ]
+        }
+    ]
+};
+
+module.exports = autos;
