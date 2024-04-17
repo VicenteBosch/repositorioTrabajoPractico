d7fcdc71e13e8740fe6c9240449a54cb9d841952
Bautista Juarez <bjuarez@udesa.edu.ar>
2024-04-17 14:28:44 -0300

    detalle

--- tp/db/index.js
+++ tp/db/index.js
@@ -28,7 +28,7 @@ const autos = {
             comentarios: [
                 {
                     nombre_usuario: "Usuario2",
-                    texto_comentario: "Comentario sobre Honda Civic",
+                    texto: "Comentario sobre Honda Civic",
                     imagen_perfil: "/img/perfil2.jpg"
                 }
             ]
@@ -40,7 +40,7 @@ const autos = {
             comentarios: [
                 {
                     nombre_usuario: "Usuario3",
-                    texto_comentario: "Comentario sobre Ford Fiesta",
+                    texto: "Comentario sobre Ford Fiesta",
                     imagen_perfil: "/img/perfil3.jpg"
                 }
             ]
@@ -52,7 +52,7 @@ const autos = {
             comentarios: [
                 {
                     nombre_usuario: "Usuario4",
-                    texto_comentario: "Comentario sobre Chevrolet Cruze",
+                    texto: "Comentario sobre Chevrolet Cruze",
                     imagen_perfil: "/img/perfil4.jpg"
                 }
             ]
@@ -64,67 +64,67 @@ const autos = {
             comentarios: [
                 {
                     nombre_usuario: "Usuario5",
-                    texto_comentario: "Comentario sobre Nissan Sentra",
+                    texto: "Comentario sobre Nissan Sentra",
                     imagen_perfil: "/img/perfil5.jpg"
                 }
             ]
         },
         {
-            imagen: "/img/auto6.jpg",
-            producto: "Volkswagen Jetta",
+            imagen: "/images/products/auto1.jpg",
+            producto: "Volkswagen Gol",
             descripcion: "Descripción del Volkswagen Jetta",
             comentarios: [
                 {
                     nombre_usuario: "Usuario6",
-                    texto_comentario: "Comentario sobre Volkswagen Jetta",
+                    texto: "Comentario sobre Volkswagen Jetta",
                     imagen_perfil: "/img/perfil6.jpg"
                 }
             ]
         },   
         {
-            imagen: "/img/auto7.jpg",
-            nombre_producto: "Hyundai Elantra",
+            imagen: "/images/products/auto1.jpg",
+            producto: "Hyundai Elantra",
             descripcion: "Descripción del Hyundai Elantra",
             comentarios: [
                 {
                     nombre_usuario: "Usuario7",
-                    texto_comentario: "Comentario sobre Hyundai Elantra",
+                    texto: "Comentario sobre Hyundai Elantra",
                     imagen_perfil: "/img/perfil7.jpg"
                 }
             ]
         },
         {
-            imagen: "/img/auto8.jpg",
-            nombre_producto: "Mazda3",
+            imagen: "/images/products/auto1.jpg",
+            producto: "Mazda3",
             descripcion: "Descripción del Mazda3",
             comentarios: [
                 {
                     nombre_usuario: "Usuario8",
-                    texto_comentario: "Comentario sobre Mazda3",
+                    texto: "Comentario sobre Mazda3",
                     imagen_perfil: "/img/perfil8.jpg"
                 }
             ]
         },
         {
-            imagen: "/img/auto9.jpg",
-            nombre_producto: "Subaru Impreza",
+            imagen: "/images/products/auto1.jpg",
+            producto: "Subaru Impreza",
             descripcion: "Descripción del Subaru Impreza",
             comentarios: [
                 {
                     nombre_usuario: "Usuario9",
-                    texto_comentario: "Comentario sobre Subaru Impreza",
+                    texto: "Comentario sobre Subaru Impreza",
                     imagen_perfil: "/img/perfil9.jpg"
                 }
             ]
         },
         {
             imagen: "/img/auto10.jpg",
-            nombre_producto: "Kia Forte",
+            producto: "Kia Forte",
             descripcion: "Descripción del Kia Forte",
             comentarios: [
                 {
                     nombre_usuario: "Usuario10",
-                    texto_comentario: "Comentario sobre Kia Forte",
+                    texto: "Comentario sobre Kia Forte",
                     imagen_perfil: "/img/perfil10.jpg"
                 }
             ]
