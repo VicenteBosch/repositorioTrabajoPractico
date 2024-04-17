e05abaf24e08a32ddc796c1fa3206d779968d147
VicenteBosch <vbosch@udesa.edu.ar>
2024-04-17 15:52:21 -0300

    Agregando autos a las pagina, y cambios en el css

--- tp/db/index.js
+++ tp/db/index.js
@@ -79,7 +79,7 @@ const autos = {
             ]
         },   
         {
-            imagen: "/images/products/auto1.jpg",
+            imagen: "/images/products/auto5.jpg",
             producto: "Nissan Sentra",
             descripcion: "Descripción del Nissan Sentra",
             comentarios: [
@@ -91,7 +91,7 @@ const autos = {
             ]
         },
         {
-            imagen: "/images/products/auto1.jpg",
+            imagen: "/images/products/auto6.jpg",
             producto: "Volkswagen Gol",
             descripcion: "Descripción del Volkswagen Jetta",
             comentarios: [
@@ -103,49 +103,73 @@ const autos = {
             ]
         },   
         {
-            imagen: "/images/products/auto1.jpg",
-            producto: "Hyundai Elantra",
-            descripcion: "Descripción del Hyundai Elantra",
+            imagen: "/images/products/auto7.jpg",
+            producto: "Volkswagen Taos",
+            descripcion: "Descripción del volkswagen taos",
             comentarios: [
                 {
                     nombre_usuario: "Usuario7",
-                    texto: "Comentario sobre Hyundai Elantra",
+                    texto: "Comentario sobre volkswagen taos",
                     imagen_perfil: "/img/perfil7.jpg"
                 }
             ]
         },
         {
-            imagen: "/images/products/auto1.jpg",
-            producto: "Mazda3",
-            descripcion: "Descripción del Mazda3",
+            imagen: "/images/products/auto8.jpg",
+            producto: "Toyota Hilux",
+            descripcion: "Descripción del Toyota Hilux",
             comentarios: [
                 {
                     nombre_usuario: "Usuario8",
-                    texto: "Comentario sobre Mazda3",
+                    texto: "Comentario sobre Toyota Hilux",
                     imagen_perfil: "/img/perfil8.jpg"
                 }
             ]
         },
         {
-            imagen: "/images/products/auto1.jpg",
-            producto: "Subaru Impreza",
-            descripcion: "Descripción del Subaru Impreza",
+            imagen: "/images/products/auto9.jpg",
+            producto: "Dodge Journey",
+            descripcion: "Descripción del Dodge Journey",
             comentarios: [
                 {
                     nombre_usuario: "Usuario9",
-                    texto: "Comentario sobre Subaru Impreza",
+                    texto: "Comentario sobre Dodge Journey",
                     imagen_perfil: "/img/perfil9.jpg"
                 }
             ]
         },
         {
             imagen: "/img/auto10.jpg",
-            producto: "Kia Forte",
-            descripcion: "Descripción del Kia Forte",
+            producto: "Kia Sportage",
+            descripcion: "Descripción del Kia Sportage",
             comentarios: [
                 {
                     nombre_usuario: "Usuario10",
-                    texto: "Comentario sobre Kia Forte",
+                    texto: "Comentario sobre Kia Sportage",
+                    imagen_perfil: "/img/perfil10.jpg"
+                }
+            ]
+        },
+        {
+            imagen: "/images/products/auto11.jpg",
+            producto: "Toyota Hilux",
+            descripcion: "Descripción del Toyota Hilux",
+            comentarios: [
+                {
+                    nombre_usuario: "Usuario11",
+                    texto: "Comentario sobre Toyota Hilux",
+                    imagen_perfil: "/img/perfil9.jpg"
+                }
+            ]
+        },
+        {
+            imagen: "/img/auto12.jpg",
+            producto: "Toyota Yaris",
+            descripcion: "Descripción del Toyota Yaris",
+            comentarios: [
+                {
+                    nombre_usuario: "Usuario12",
+                    texto: "Comentario sobre Toyota Yaris",
                     imagen_perfil: "/img/perfil10.jpg"
                 }
             ]
