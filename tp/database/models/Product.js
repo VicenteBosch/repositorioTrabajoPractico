91c7a89caf718d74a7631a85c89fab4843f8c64c
Bautista Juarez <bjuarez@udesa.edu.ar>
2024-06-05 21:21:22 -0300

    modelo de producto, falta corregir un error que no logre sacarlo

--- tp/database/models/Product.js
+++ tp/database/models/Product.js
@@ -1,18 +1,53 @@
-module.exports = function (sequelize , dataTypes) {
-    let alias = "Product";
+module.exports = function (sequelize, DataTypes) {
 
+    let alias = 'Product';
+  
     let cols = {
-        id_producto: {
-            autoIncrement: true,
-            primaryKey: true,
-            type: dataTypes.INTEGER,
-        },
+      id_producto: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      id_usuarios: {
+        type: DataTypes.INTEGER,
+
+      },
+      nombre_producto: {
+        type: DataTypes.STRING,
         
-    
-    }
+      },
+      descripcion_producto: {
+        type: DataTypes.STRING,
+        
+      },
+      nombre_archivo_imagen: {
+        type: DataTypes.STRING,
+        
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+       
+      },
+    };
+  
+    let config = {
+      tableName: 'productos',
+      timestamps: true,
+      underscored: true,
+    };
+  
+    let Product = sequelize.define(alias, cols, config);
+  
+  
+    return Product;
 
-
-
-
-
-}
+  };
+  
