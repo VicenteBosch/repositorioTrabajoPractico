e83a5a5f7e6cdc827fce31cb933c010d0125c03d
Bautista Juarez <bjuarez@udesa.edu.ar>
2024-06-11 16:10:46 -0300

    detalles

--- tp/routes/profile.js
+++ tp/routes/profile.js
@@ -6,7 +6,6 @@ const registerValidtions = require('../middlewares/register-validator');
 router.get("/" , profileController.profile);
 router.get("/edit" , profileController.profileEdit);
 router.get("/login" , profileController.login);
-
 router.get("/register" , profileController.register)
 router.post("/register" , registerValidtions, profileController.store);
 
